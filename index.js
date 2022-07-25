83377220e8cdaa5b356f890257abbb619d017868
Sam <103090422+brave-sam@users.noreply.github.com>
2022-07-26 03:45:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const {
+  Client,
+  GatewayIntentBits,
+  Partials,
+  Collection,
+} = require("discord.js");
+
+const { Guilds, GuildMembers, GuildMessages } = GatewayIntentBits;
+const { User, Message, GuildMember, ThreadMember } = Partials;
+
+const { loadEvents } = require("./Handlers/eventHandler");
+const { loadCommands } = require("./Handlers/commandHandler");
+
+const client = new Client({
+  intents: [Guilds, GuildMembers, GuildMessages],
+  partials: [User, Message, GuildMember, ThreadMember],
+});
+
+client.commands = new Collection();
+client.config = require("./config.json");
+client
+  .login(client.config.token)
+  .then(() => {
+    loadEvents(client);
+    loadCommands(client);
+    client.user.setActivity("With LeGION_V14");
+  })
+  .catch((err) => console.log(err));
